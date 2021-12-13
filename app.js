4c0243d10743645bce25299d929e5d3c3872ee84
8b477 <jonathan_buchet@outlook.fr>
2021-12-13 11:54:11 +0100

    => passer le tout en mode objet !

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
+let stopButton = document.getElementById("stop-button");
 
+//compte a rebours créer par l'utilisateur
+//récup les input
 let inputSecondes = document.getElementById('sec');
 let inputMinutes = document.getElementById('min');
 let inputHeures = document.getElementById('hour');
 
+//récup le span pour afficher le décompte
+let spanSec = document.getElementById('secSpan');
+let spanMin = document.getElementById('secMin');
+let spanHour = document.getElementById('secHour');
 
-let count = 0;
-let btnStart = document.getElementById('start');
-let stopButton = document.getElementById("stop-button");
-let seconds = document.getElementById("seconds");
+//récup les btn
+let btnUserStart = document.getElementById('startUser');
 
-btnStart.addEventListener('click',function recu() {
-    let interval = setTimeout(recu,1000, count);
-    count++;
-    seconds.innerHTML = count.toString();
+//start le décompte
+btnUserStart.addEventListener('click', function recuTwo(){
+    let interval = setTimeout(recuTwo,1000, inputSecondes.value);
+    inputSecondes.value--;
+    spanSec.innerHTML = "Secondes restante : " + inputSecondes.value.toString();
 
-    stopButton.addEventListener("click", function (){
-        clearInterval(interval)
+    inputMinutes.value--;
+    spanMin.innerHTML =  "Minutes restante : " + inputMinutes.value.toString();
+
+    inputHeures.value--;
+    spanHour.innerHTML = "Heures restante : " + inputHeures.value.toString();
+
+    if (inputSecondes.value === '0'){
+        clearInterval(interval);
+
+    }
+    if (inputMinutes.value === '0'){
+        clearInterval(interval);
+    }
+    if (inputHeures.value === '0'){
+        clearInterval(interval);
+    }
+
+//clear le décompte
+    stopButton.addEventListener('click', function (){
+        clearInterval(interval);
+        inputSecondes.value = '0';
+        spanSec.innerHTML = "";
+
+        inputMinutes.value = '0';
+        spanMin.innerHTML = "";
+
+        inputHeures.value = '0';
+        spanHour.innerHTML = "";
     })
 });
